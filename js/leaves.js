831c2d86ef8eb70d07b7e803738ab71e0782ae33
Ender <wmh19590120@163.com>
2024-04-16 21:41:25 +0800

    Site updated: 2024-04-16 21:41:24

--- js/leaves.js
+++ js/leaves.js
@@ -109,7 +109,7 @@ function startSakura() {
     document.getElementsByTagName("body")[0].appendChild(canvas);
     cxt = canvas.getContext("2d");
     var sakuraList = new SakuraList();
-    for (var i = 0; i < 30; i++) {
+    for (var i = 0; i < 20; i++) {
         var sakura, randomX, randomY, randomS, randomR, randomFnx, randomFny;
         randomX = getRandom("x");
         randomY = getRandom("y");
