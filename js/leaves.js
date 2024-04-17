f459983fdb9b6feac1a6f37033fdd7d5827ffda6
Ender <wmh19590120@163.com>
2024-04-17 11:21:57 +0800

    Site updated: 2024-04-17 11:21:56

--- js/leaves.js
+++ js/leaves.js
@@ -70,7 +70,7 @@ function getRandom(option) {
             ret = Math.random() * window.innerHeight;
             break;
         case "s":
-            ret = 0.6 + Math.random() * 0.5;
+            ret = 1 + Math.random() * 0.5;
             break;
         case "r":
             ret = Math.random() * 4;
